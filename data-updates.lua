2cbd365c6da244dd1bc435b6cb86da2e4979cfc0
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-10-16 00:21:14 +0200

    release v2.0.3

--- data-updates.lua
+++ data-updates.lua
@@ -2,6 +2,6 @@ local range = settings.startup['rras_range'].value or 23
 
 for _, sub_type in pairs({'rail-ramp', 'rail-support'}) do
   for _, prototype in pairs(data.raw[sub_type]) do
-    prototype.support_range = 23--range
+    prototype.support_range = range
   end
 end
